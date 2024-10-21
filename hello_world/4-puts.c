cd828fad659b7b9f1b9b94dffde3f54468020dce
Myriam <myriiamwayne@gmail.com>
2024-10-21 15:00:04 +0200

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * print_hello - Prints "Programming is like building a multilingual puzzle"
+ * print_hello - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  * Return: 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return 0;
 }
