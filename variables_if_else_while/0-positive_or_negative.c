6c3a006bbb40f7ce8f8869c88e911bce96b3ba0c
Myriam <myriiamwayne@gmail.com>
2024-10-22 14:17:30 +0200

    positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int main(void)
 
 	srand(time(0));
 	n= rand() - RAND_MAX / 2;
-
+	printf("%d \n", n);
 
 
 if (n>0) {	
-	printf("is positive");
+	printf("is positive\n");
 }else (n==0){
-	printf("is zero")
+	printf("is zero\n")
 }else if{
-	printf("is negative")
+	printf("is negatie\n")
 }
  	return (0);
 }
