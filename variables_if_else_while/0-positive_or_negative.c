9e1c98166c28ade6eee85dcd7bafd05f7b9bb60a
Myriam <myriiamwayne@gmail.com>
2024-10-22 14:07:55 +0200

    positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,14 @@
  *main -  program will assign a random number to the,
  variable n each time it is executed.
  */
-int main(void){
+int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n= rand() - RAND_MAX / 2;
 
-}
+
 
 if (n>0) {	
 	printf("is positive");
