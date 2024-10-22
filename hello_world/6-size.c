e932c3eec1eb369c3cdd2d557c874fd84be52773
Myriam <myriiamwayne@gmail.com>
2024-10-22 09:30:59 +0200

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types on the computer it is compiled
+ * and run on.
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	Size of a char: 1 byte(s)
+	Size of an int: 4 byte(s)
+	Size of an long int: 4 byte(s)
+	Size of a long long int: 8 byte(s);
+	Size of a float: 4 byte(s)
+
+	return (0);
+}
