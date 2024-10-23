645a9d5550d7bd6d91be5021c9bf67efef3f9ea2
Myriam <myriiamwayne@gmail.com>
2024-10-23 21:18:35 +0200

    print base

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,19 +5,25 @@
 */
 int main(void)
 {
-	int i = 48;
+	int i;
 
+	for
+		(i = 48)
+	{
 	if
 		(i < 58)
 	{
 		putchar(i);
 		i++;
 	}
-	else
-		(i > 96 ; i < 103)
+	for
+		(i = 96)
+	if
+		(i < 103)
 	{
 		putchar(i);
 		i++;
+	}
 	}
 	putchar('\n');
 	return (0);
