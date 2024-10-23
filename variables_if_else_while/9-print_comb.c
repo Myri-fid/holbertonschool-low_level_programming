f3ee0564d21d3a1839e4890c0231b295bf656a6a
Myriam <myriiamwayne@gmail.com>
2024-10-23 20:25:54 +0200

    print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+        int i = 48;
+
+        for
+                (i < 58)
+        {
+                putchar(i);
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}
