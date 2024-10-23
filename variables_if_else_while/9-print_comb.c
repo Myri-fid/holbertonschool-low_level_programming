8e2a844cf70c7eefd23b390530c4e283c208fbcd
Myriam <myriiamwayne@gmail.com>
2024-10-23 20:28:52 +0200

    print comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-        int i = 48;
+	int i = 48;
 
-        for
-                (i < 58)
-        {
-                putchar(i);
-                i++;
-        }
-        putchar('\n');
-        return (0);
+	while
+		(i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
