f1c14f6d3a783344bf10474dde752366ac50d6f8
Myriam <myriiamwayne@gmail.com>
2024-10-23 13:02:20 +0200

    print numeber

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	n = 10;
+	{
+		printf("%d", n - 1);
+	}
+	return (0);
+}
