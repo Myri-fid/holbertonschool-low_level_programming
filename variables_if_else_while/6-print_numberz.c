e46151a0a37919bd58fef39bf33a1b5617276804
Myriam <myriiamwayne@gmail.com>
2024-10-23 19:09:23 +0200

    numbrz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+i = 0;
+
+	while
+		(i < 10)
+	{
+		putchar('%d', i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
