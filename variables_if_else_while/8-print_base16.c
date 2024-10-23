9cfc7b286f481bcd47d810ef7a2494d8c29de8ce
Myriam <myriiamwayne@gmail.com>
2024-10-23 19:59:28 +0200

    print base

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	if
+		(i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	else
+		(i > 96 && i < 103)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
