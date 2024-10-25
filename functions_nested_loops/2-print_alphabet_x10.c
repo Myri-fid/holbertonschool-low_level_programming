c5c0b695bc9e5f786868f08f5ac858c28b684c49
Myriam <myriiamwayne@gmail.com>
2024-10-25 15:23:11 +0200

    alphabetdix

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - check the code
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' ; letter++)
+
+	if
+		(letter == 10)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
