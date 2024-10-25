ae187fe36ba77490287d215756e700f0bbed3cf1
Myriam <myriiamwayne@gmail.com>
2024-10-25 14:26:05 +0200

    prt

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,7 +9,6 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(num + '0');
 	if
 		(num == 9)
 	{
@@ -17,7 +16,6 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
-
 	}
 	putchar('\n');
 	return (0);
