33dc910d39c82781067fb88db1be53e4cfe37381
Myriam <myriiamwayne@gmail.com>
2024-10-25 22:52:44 +0200

    isal

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*_isalpha - check if a character is an alphabetic character.
-*
+*_isalpha - check if is an alphabetic character.
+* @c: The character to check (as an integer).
 * Return: 1 if c is lowercase, 0 otherwise.
 */
 int _isalpha(int c)
