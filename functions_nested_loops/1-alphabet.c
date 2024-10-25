3785eb82c69b25c2ab78c856265045564ded20ca
Myriam <myriiamwayne@gmail.com>
2024-10-25 11:24:36 +0200

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - _putchar charactere
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int alphabet;
+
+	for
+		(alphabet = 'a' && alphabet <= 'z', alphabet++)
+	{
+			_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
