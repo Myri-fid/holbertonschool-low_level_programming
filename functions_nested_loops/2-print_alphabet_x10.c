116bdb7ad693419b3b764a805b5bd5e1718851f9
Myriam <myriiamwayne@gmail.com>
2024-10-25 21:39:52 +0200

    alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,15 @@
 */
 void print_alphabet_x10(void)
 {
+	int i;
 	char letter;
 
+	for (i = 0; i < 10; i++)
+	{
 	for (letter = 'a'; letter <= 'z' ; letter++)
-
-	if
-		(letter == 10)
 	{
 		_putchar(letter);
 	}
 	_putchar('\n');
+}
 }
