680d216339a9c7b9486e87ab0550b69c2e76b958
Myriam <myriiamwayne@gmail.com>
2024-10-26 21:12:49 +0200

    times

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* time_table - prints the 9 times table, starting with 0
-* @c: The character to print
+* times_table - prints the 9 times table, starting with 0
+*
 */
 void times_table(void)
 {
