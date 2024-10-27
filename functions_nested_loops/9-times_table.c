f5b0c4a3b2c4915c3baca928253c73e42e9a232d
Myriam <myriiamwayne@gmail.com>
2024-10-27 14:18:08 +0100

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-	product = i * j;
+		product = i * j;
 
 	if (j == 0)
 	{
@@ -25,7 +25,11 @@ void times_table(void)
 	{
 		_putchar(' ');
 	}
-		_putchar(product + '0');
+	if (product >= 10)
+	{
+		_putchar((product / 10) + '0');
+	}
+		_putchar((product % 10) + '0');
 	}
 	}
 	_putchar('\n');
