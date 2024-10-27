89ea9a90c0e5c5fba98cd0bef03240a69bfa994b
Myriam <myriiamwayne@gmail.com>
2024-10-27 14:08:06 +0100

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,28 +5,30 @@
 */
 void times_table(void)
 {
-	int i, j;
-	int product = i * j;
+	int i, j, product;
 
-	for (int i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-	for (int j = 0; j <= 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
-	if (j != 0)
+	product = i * j;
+
+	if (j == 0)
+	{
+		_putchar(product + '0');
+	}
+	else
 	{
 		_putchar(',');
 		_putchar(' ');
-	}
 	if (product < 10)
 	{
 		_putchar(' ');
 	}
-	if (product >= 10)
-	{
-		_putchar((product / 10) + '0');
+		_putchar(product + '0');
 	}
-		_putchar((product % 10) + '0');
 	}
 	_putchar('\n');
 	}
 }
+
