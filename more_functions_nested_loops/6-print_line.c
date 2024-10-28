e13c2f47aafb192c8258c3e3c7ef228d262d6659
Myriam <myriiamwayne@gmail.com>
2024-10-28 18:46:14 +0100

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,18 +2,22 @@
 /**
 *print_line - draws a straight line in the terminal.
 *if n is 0 or less, the function should only print \n
-*@n: is the number of times the character _
-* Return: Always 0.
+*@n: is the number of times the character '_'
 */
 void print_line(int n)
 {
-	int n;
+	int i;
 
-	n = 95;
-	for (n == 95)
+	if (n <= 0)
 	{
-		_putchar(n);
-		n++;
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
 	_putchar('\n');
+	}
 }
