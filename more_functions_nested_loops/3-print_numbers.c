080665fbcc2447b71de777c718c8d7456f77d7b9
Myriam <myriiamwayne@gmail.com>
2024-10-28 14:57:27 +0100

    num

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num= 0; num <= 0; num++)
+	for (num = 0; num <= 10; num++)
 	{
-		_putchar("%d",num);
+		_putchar(num + '0');
 	}
 	_putchar('\n');
-	return (0);
 }
