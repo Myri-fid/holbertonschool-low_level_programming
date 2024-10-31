9f77cd2a4564270834505956de9edc1b13ef720c
Myriam <myriiamwayne@gmail.com>
2024-10-31 02:11:12 +0100

    rev string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * rev_string - check the code
+ *@s : string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int end = 0;
+	int start = 0;
+
+	while (s[end] != '\0')
+	{
+		end++;
+	}
+	end--;
+	while (end > start)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+	write(1, "\n", 1);
+}
