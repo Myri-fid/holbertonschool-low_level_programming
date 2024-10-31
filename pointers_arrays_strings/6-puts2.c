20bf52c9696c7951c279a3435784906d605cd3ca
Myriam <myriiamwayne@gmail.com>
2024-10-31 18:00:27 +0100

    puts pair

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 #include <stdio.h>
 /**
  * puts2 - prints every other character of a string
@@ -9,18 +8,15 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int j = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	for (; j < i; j++)
+	if (i % 2 == 0)
 	{
-	if (j % 2 == 0)
-	{
-		putchar(str[j]);
+		putchar(str[i]);
 	}
-	}
+	i++;
 	putchar('\n');
 }
