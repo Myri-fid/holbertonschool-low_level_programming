8366119171ad0f27c58d857684ae910c1afc2dea
Myriam <myriiamwayne@gmail.com>
2024-11-02 17:00:17 +0100

    print array

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
  *
@@ -7,9 +6,13 @@
  */
 int main(void)
 {
-	char *str;
+	int array[5];
 
-	str = "0123456789";
-	puts2(str);
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	print_array(array, 5);
 	return (0);
 }
