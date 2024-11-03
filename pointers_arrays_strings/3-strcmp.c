90e9e76b45e529f1a9d205a922061bbf03c43bc2
Myriam <myriiamwayne@gmail.com>
2024-11-03 18:57:39 +0100

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,27 +5,26 @@
  * _strcmp - Copies the string pointed to by src to dest
  * @s1: destination buffer
  * @s2: string
- * @n: bytes from src
  * Return: A pointer to dest
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 > 0 && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-	}
-	if (*s1 == *s2)
-	{
-		*s1 = '\0';
-		*s2 = '\0';
-	}
-	else if (*s1 < *s2)
-	{
-		s1--;
-		s2--;
-	}
-	else(*s1 > *s2)
-	{
-		s1++;
-		s2++;
+		if (*s1 == *s2)
+		{
+			*s1 = '\0';
+			*s2 = '\0';
+		}
+		else if (*s1 < *s2)
+		{
+			s1--;
+			s2--;
+		}
+		else
+		{
+			s1++;
+			s2++;
+		}
 	}
 }
