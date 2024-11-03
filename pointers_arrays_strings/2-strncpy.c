39a123d9010e9ce6a46201f52115a1faa2de6828
Myriam <myriiamwayne@gmail.com>
2024-11-03 16:59:48 +0100

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Copies the string pointed to by src to dest
+ * @dest: destination buffer
+ * @src: string
+ * @n: bytes from src
+ * Return: A pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (n > 0 && *src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+	return (dest);
+	*ptr = '\0';
+}
