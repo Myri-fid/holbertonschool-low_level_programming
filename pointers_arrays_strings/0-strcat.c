b9f30d7bde9c840e765860361799ecf97ee49aa3
Myriam <myriiamwayne@gmail.com>
2024-11-03 15:37:23 +0100

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* _strcat - Copies the string pointed to by src to dest
-* @dest: destination buffer
-* @src: string
-*
-* Return: A pointer to dest
-*/
+ * _strcat - Copies the string pointed to by src to dest
+ * @dest: destination buffer
+ * @src: string
+ *
+ * Return: A pointer to dest
+ */
 char *_strcat(char *dest, char *src)
 {
-	size_t dest_len = strlen(dest);
+	char *ptr = dest;
 
-	while (*src)
+	while (*ptr != '\0')
 	{
-		dest[dest_len] = *src;
-		dest[dest_len] = '\0';
+		ptr++;
 	}
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
 	return (dest);
 }
