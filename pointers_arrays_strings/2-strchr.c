74cb06f1cec90bb3086155e35399c8693c96d4fb
Myriam <myriiamwayne@gmail.com>
2024-11-04 14:49:10 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,10 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _strchr - check the code
+ *@c: character
+ *@s:string
  * Return: Always 0.
  */
 char *_strchr(char *s, char c)
 {
+	int i;
+
+	for (i = 0; i < c; i++)
+	{
+	if (c != *s)
+		return ('\0');
+	}
+	return (s);
 }
