8fded919deda2c6ac0b48bbd06e18b9382ed7b10
Myriam <myriiamwayne@gmail.com>
2024-11-05 14:59:28 +0100

    print chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,16 @@
 */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; a[i][7]; i++)
 	{
 	for (j = 0; j < 8; j++)
 	{
-	printf("%c ", a[i][j]);
+		_putchar (a[i][j]);
 	}
-	printf("\n");
+	_putchar ('\n');
 	}
+
 }
