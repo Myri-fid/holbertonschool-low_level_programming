c1fc47bfaf0a3a4a763db1c1caadce2fab2c9e5d
Myriam <myriiamwayne@gmail.com>
2024-11-05 16:31:05 +0100

    print diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * print_diagsums - check the code
+ *@a: diagonal
+ *@size: size
  * Return: Always 0.
  */
 void print_diagsums(int *a, int size)
 {
+	int i;
+	long int sum1 = 0;
+	long int sum2 = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%ld, %ld\n", sum1, sum2);
+}
