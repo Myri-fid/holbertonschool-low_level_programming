e6b5f0866324f0beef94b66c8e235682d7c0c7a7
Myriam <myriiamwayne@gmail.com>
2024-11-07 19:19:18 +0100

    vv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,13 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	(void)argc;
+	printf("%s\n", argv[0]);
 
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-		putchar(argv[0][i]);
-	}
-	putchar('\n');
-	return (0);
+	return 0;
 }
