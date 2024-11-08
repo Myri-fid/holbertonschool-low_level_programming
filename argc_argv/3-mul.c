6303c8b598e71c33e5725f1688c34109df649363
Myri-fid <myriamguillabert@hotmail.fr>
2024-11-08 17:10:12 +0100

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,15 +12,15 @@
  */
 int main(int argc, char *argv[])
 {
-  int x = atoi(argv[1]);
-  int y = atoi(argv[2]);
-  int cal = x *y
+      int x = atoi(argv[1]);
+      int y = atoi(argv[2]);
+      int cal = x *y
 
-  if ( argc < 3 )
-  {
-    printf( "Error\n");
-    return (1);
-  }
-  printf("%d.\n", cal);
-  return(0);
+      if ( argc < 3 )
+      {
+            printf( "Error\n");
+             return (1);
+      }
+      printf("%d.\n", cal);
+      return(0);
 }
