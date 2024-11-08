fc5ac83cbb1d4cdbdf6e91448243bf9ed9f2db86
Myri-fid <myriamguillabert@hotmail.fr>
2024-11-08 17:06:54 +0100

    Create 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the number of argument
+ * @argc: The number of arguments
+ * @argv: An array of strings
+ *@x: num
+ *@y: num
+ *@cal: calcul
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+  int x = atoi(argv[1]);
+  int y = atoi(argv[2]);
+  int cal = x *y
+
+  if ( argc < 3 )
+  {
+    printf( "Error\n");
+    return (1);
+  }
+  printf("%d.\n", cal);
+  return(0);
+}
