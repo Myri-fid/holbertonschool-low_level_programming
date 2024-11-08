9e16b9d49ab79a56f01bea75783bfafff04a0de0
Myri-fid <myriamguillabert@hotmail.fr>
2024-11-08 17:22:41 +0100

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
       if ( argc < 3 )
       {
-            printf( "Error\n");
+            printf( "Error\n", argv[0]);
              return (1);
       }
       printf("%d.\n", cal);
