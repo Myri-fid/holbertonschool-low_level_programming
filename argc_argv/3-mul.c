e005dd231ab565dbbd74047e2fe084d685b33201
Myriam <myriiamwayne@gmail.com>
2024-11-12 18:48:50 +0100

    mulc

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,20 +12,20 @@
  */
 int main(int argc, char *argv[])
 {
-            int x = 0;
-            int y = 0;
+	int x = 0;
+	int y = 0;
 
-            if (argc == 3)
-            {
-                        x = atoi(argv[1]);
-                        y = atoi(argv[2]);
-                        printf("%d\n", x * j);
-            }
-            else
-            {
-                        printf("Error\n");
-                        return (1);
-            }
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-            return (0);
+	return (0);
 }
