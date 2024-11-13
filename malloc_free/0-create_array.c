e71aed760e3d53ba07c1b9c4118cabc55ab4e7fc
Myriam <myriiamwayne@gmail.com>
2024-11-13 11:12:50 +0100

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,8 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = ((char *)malloc(size));
-
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	if (size == 0)
 	{
 		return (NULL);
