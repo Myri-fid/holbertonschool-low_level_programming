6f62a5d95e605cd8276ec47b4a95c189c3300d15
Myriam <myriiamwayne@gmail.com>
2024-11-14 16:17:23 +0100

    string nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 /**
-*string_nconcat - concatenates two strings
-*@n: nombre
-*@s1: string
-*@s2: string
-*Return: null is passed, treat it as an empty string
-*/
+ *string_nconcat - concatenates two strings
+ *@n: nombre
+ *@s1: string
+ *@s2: string
+ *Return: null is passed, treat it as an empty string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *nconcat;
@@ -16,23 +16,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lens2 = 0;
 	unsigned int i;
 
-	if (n >= lens2)
-	{
-		s2++;
-	}
-	if (s1 == NULL || s2 == NULL)
-	{
-		return ("");
-	}
-	while (s1[lens1] != '\0' || s2[lens2] != '\0')
-	{
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[lens1] != '\0')
 		lens1++;
+	while (s2[lens2] != '\0')
 		lens2++;
-	}
+	if (n > lens2)
+		n = lens2;
 	nconcat = malloc(sizeof(char) * (lens1 + n + 1));
 	if (nconcat == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < lens1; i++)
+	{
+		nconcat[i] = s1[i];
 	}
 	for (i = 0; i < n; i++)
 	{
