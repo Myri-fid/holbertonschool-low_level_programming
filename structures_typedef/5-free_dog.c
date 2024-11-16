191d1249d7916edd30630c77d63593535527715c
Myriam <myriiamwayne@gmail.com>
2024-11-16 19:19:57 +0100

    free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Check the code
+ * @d: dog
+ * Return: NULL
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
