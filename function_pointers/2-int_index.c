5454fcfd86feb5ec7ef6c600e4e5c07989ca5c40
Myriam <myriiamwayne@gmail.com>
2024-11-18 12:01:35 +0100

    int index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: the integer to check
+ * @size: size
+ * @cmp: cmp
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	
+	if (array && cmp && size > 0)
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
