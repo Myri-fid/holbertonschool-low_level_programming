40d66f2b23437ba1e591306e9ef043b1b3b34918
Myriam <myriiamwayne@gmail.com>
2024-11-19 14:19:36 +0100

    print number

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,4 +8,20 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	int num;
 
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		if (separator != NULL && i > 0)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", num);
+	}
+	printf("\n");
+	va_end(args);
+}
