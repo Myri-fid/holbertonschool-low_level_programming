ec4a96fbcee12b1f51d661f138f8106a2e831aeb
Myriam <myriiamwayne@gmail.com>
2024-11-22 22:41:07 +0100

    function

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,63 +1,70 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "calc.h"
+
 /**
- * op_add - calcul
- * @a: number
- * @b: number
- * Return: value
+ * op_add - Effectue l'addition de deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: La somme de a et b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - calcul
- * @a: number
- * @b: number
- * Return: value
+ * op_sub - Effectue la soustraction de deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: La différence entre a et b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - calcul
- * @a: number
- * @b: number
- * Return: value
+ * op_mul - Effectue la multiplication de deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: Le produit de a et b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - calcul division
- * @a: number
- * @b: number
- * Return: value
+ * op_div - Effectue la division de deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: Le quotient de a divisé par b, ou 0 si b est égal à 0
  */
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
-		exit(100);
+		return (0);  /* Gère la division par zéro */
 	}
 	return (a / b);
 }
+
 /**
- * op_mod - calcul modulo
- * @a: number
- * @b: number
- * Return: value
+ * op_mod - Effectue le modulo de deux entiers
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: Le reste de a divisé par b, ou 0 si b est égal à 0
  */
 int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
-		exit(100);
+		return (0);  /* Gère la division par zéro */
 	}
 	return (a % b);
 }
+
