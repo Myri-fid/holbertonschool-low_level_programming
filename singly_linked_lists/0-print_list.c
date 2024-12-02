cd0ec47e41d479aaf0ff85e8913e951bf56184d1
Myriam <myriiamwayne@gmail.com>
2024-12-02 15:58:54 +0100

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ *@h : head
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	int n_noeud = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		n_noeud++;
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d][%s]\n", current->len, current->str);
+		current = current->next;
+	}
+	printf("\n");
+	return (n_noeud);
+}
