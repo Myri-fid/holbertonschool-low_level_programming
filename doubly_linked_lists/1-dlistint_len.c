354f7e35ced3f0df0669c74241d5a2ade8e2e264
Myriam <myriiamwayne@gmail.com>
2024-12-04 13:10:40 +0100

    double node

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,27 +4,19 @@
 #include "lists.h"
 
 /**
- * print_dlistint - check the code
+ * dlistint_len - check the code
  *@h: head
  * Return: Always EXIT_SUCCESS.
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
 
 	while (current != NULL)
 	{
-		printf("%d", current->n);
-
-		if (current->next != NULL)
-		{
-			printf("\n");
-		}
-
 		current = current->next;
 		count++;
 	}
-	printf("\n");
 	return (count);
 }
