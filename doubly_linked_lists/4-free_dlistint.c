2d9b0ec15aa5f2b916170200d0025d87b5985a33
Myriam <myriiamwayne@gmail.com>
2024-12-04 14:09:34 +0100

    free double links

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
