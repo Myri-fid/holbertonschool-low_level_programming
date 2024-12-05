184441cb35a6c1b0aad1c38717ddbf93dd3a42c9
Myriam <myriiamwayne@gmail.com>
2024-12-05 18:17:48 +0100

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,13 +11,13 @@
 void free_list(list_t *head)
 {
 	list_t *current = head;
-	list_t *temp;
+	list_t *next_node;
 
 	while (current != NULL)
 	{
-		temp = current;
-		current = current->next;
-		free(temp->str);
-		free(temp);
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
 	}
 }
