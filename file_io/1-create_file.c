dde242121f7d7fe7fcbf5c027f5ae8d6d018edc5
Myriam <myriiamwayne@gmail.com>
2024-12-06 19:56:35 +0100

    filename

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * create_file - check the code
+ *@text_content: filename
+ *@filename: filename
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+	int len = 0;
+
+
+	if (filename == NULL)/*si nom fichier et null*/
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT |  O_WRONLY | O_TRUNC | O_APPEND);
+	if (fd == -1)
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (-1);
+}
