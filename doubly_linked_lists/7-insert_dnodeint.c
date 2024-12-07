6f470aeec85d79bc8f08c49646ab6b346ed87b1d
Myriam <myriiamwayne@gmail.com>
2024-12-07 15:12:15 +0100

    insert dnodeint

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - check the code
+ *@idx: idx
+ *@n: n
+ *@h: h
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+	unsigned int num = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	temp = *h;
+	while (temp && num < idx - 1)
+	{
+		temp = temp->next;
+		num++;
+	}
+	if (temp == NULL || (temp->next == NULL && num + 1 != idx))
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
+}
