832f32059c668300cd11538aaac4877bad8db7d8
Myriam <myriiamwayne@gmail.com>
2025-01-06 14:26:02 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,27 +23,36 @@ void error_exit(int code, const char *message, const char *file)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_bytes;
+	int fd_from, fd_to, read_bytes, write_bytes;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		error_exit(97, " %s %s\n", "file_from file_to");
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error %s\n", argv[1]);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_exit(99, "Error %s\n", argv[2]);
+		error_exit(99, "Error: Can't write to file %s\n", argv[2]);
+
 	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (write(fd_to, buffer, read_bytes) != read_bytes)
-			error_exit(99, "Error %s\n", argv[2]);
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
+			error_exit(99, "Error: Can't write to file %s\n", argv[2]);
 	}
+
 	if (read_bytes == -1)
-		error_exit(98, "Error %s\n", argv[1]);
-	if (close(fd_from) == -1)
-		error_exit(100, "Error%d\n", fd_from);
-	if (close(fd_to) == -1)
-		error_exit(100, "Error %d\n", fd_to);
-	return (0);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	close(fd_from);
+	close(fd_to);
+
+	return 0;
 }
+
